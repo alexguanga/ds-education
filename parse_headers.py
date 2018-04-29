69706e110741cdf0b07194bb5ed1d016c4f95b29
Alex Guanga <alexguangaa@gmail.com>
2018-04-28 20:19:01 -0400

    New updates

--- parse_headers.py
+++ parse_headers.py
@@ -23,10 +23,10 @@ class SearchInformation:
         self.current_path()
 
     def current_path(self):
-        nearest_directories = self.nearest_dir()
-        self.search_directory(os.getcwd())
+        nearest_dirs = self.nearest_directories()
+        self.search_directory(os.getcwd(), nearest_dirs)
 
-    def nearest_dir(self):
+    def nearest_directories(self):
         files = [f for f in os.listdir('.') if os.path.isdir(f)]
         return self.validate_dirs(files)
 
@@ -36,15 +36,16 @@ class SearchInformation:
         return dirs
 
     ''' Searches for the directory in the root list '''
-    def search_directory(self, path):
+    def search_directory(self, *args):
+        path, uppers_funnels = args
         root_list = []
         for root, dirs, files in os.walk(path):
             root_list.append(root)
 
-        self.find_files(root_list)
+        self.find_files(root_list, uppers_funnels)
 
     ''' Looks for the approrpiate file '''
-    def find_files(self, root_directory):
+    def find_files(self, root_directory, upp_funnel):
         total_list = []
 
         for path in root_directory:
@@ -52,7 +53,7 @@ class SearchInformation:
                 files_in_path = os.listdir(path)
                 [total_list.append(self.dir_file(path, f)) for f in files_in_path if self.file_exists(f) is not None]
 
-        markdownfiles = GetMarkdownInfo(total_list)
+        markdownfiles = GetMarkdownInfo(total_list, upp_funnel)
 
     def extract_req_files(self, path):
         pass
@@ -82,16 +83,17 @@ class SearchInformation:
 
 class GetMarkdownInfo:
     ''' Converts juypter notebook to markdown. '''
-    def __init__(self, total_list):
+    def __init__(self, total_list, uppers_funnels):
         self.total_list = total_list
-        self.reading_files(self.total_list)
+        self.uppfunnel = uppers_funnels
+        self.reading_files(self.total_list, self.uppfunnel)
 
     ''' Computes the headers for the files'''
-    def reading_files(self, total_list):
+    def reading_files(self, total_list, uppfunnel):
         all_json = self.extract_markdowns(total_list)
-        sub_headers = self.markdown_subheaders(all_json)
+        sub_headers = self.markdown_subheaders(all_json, uppfunnel)
 
-        #CreateFrame(sub_headers)
+        CreateFrame(sub_headers)
 
     ''' Returning a list of all the files and their path (files that are not markdowns)'''
     def include_all_files(self, total_list):
@@ -130,12 +132,12 @@ class GetMarkdownInfo:
         return all_json_files
 
     ''' Extracting the content from the markdown cells. '''
-    def markdown_subheaders(self, headers):
+    def markdown_subheaders(self, headers, uppfunnel):
 
         sub_headers = []
         for header in headers:
             list_subheaders = []
-            update_path, new_file, source_info, file_header = self.parse_imp_data(header)
+            update_path, new_file, source_info, file_header = self.parse_imp_data(header, uppfunnel)
 
             list_subheaders.append(file_header)
 
@@ -149,11 +151,10 @@ class GetMarkdownInfo:
                         sub_headers.append([update_path, new_file, list_subheaders])
                 except IndexError:
                     pass
-
         return sub_headers
 
-    def parse_imp_data(self, header):
-        new_path_name, new_file = self.scrape_file_info(header)
+    def parse_imp_data(self, header, uppfunnel):
+        new_path_name, new_file = self.scrape_file_info(header, uppfunnel)
         all_sources_info = self.scrape_source_info(header)
         file_header = self.dir_filename(header)
         return [new_path_name, new_file, all_sources_info, file_header]
@@ -177,9 +178,9 @@ class GetMarkdownInfo:
     def scrape_source_info(self, header):
         return header[0]
 
-    def scrape_file_info(self, header):
+    def scrape_file_info(self, header, uppfunnel):
         path_name = header[1]
-        new_path, new_file = self.last_instance(path_name)
+        new_path, new_file = self.last_instance(path_name, uppfunnel)
         return [new_path, new_file]
 
     def main_topic(self, source):
@@ -194,27 +195,57 @@ class GetMarkdownInfo:
         return topic
 
     ''' Returns last instance of /, which separates directories'''
-    def last_instance(self, full_path):
-        instance_pos = full_path.rfind('/')
-        path = self.updated_pathname(full_path, instance_pos)
-        file = self.updated_file(full_path, instance_pos)
+    def last_instance(self, full_path, uppfunnel):
+        for dir in uppfunnel:
+            length = len(dir)
+            instance_pos = full_path.find(dir)
 
-        return [path, file]
+            if instance_pos != -1:
+                path = self.updated_pathname(full_path, instance_pos+length, uppfunnel)
+                file = self.updated_file(full_path, instance_pos+length)
+                return [path, file]
 
-    def updated_pathname(self, path, instance):
+    def updated_pathname(self, path, instance, uppfunnel):
         return path[0:instance]
 
     def updated_file(self, path, instance):
-        return path[instance+1:]
+        return path[instance+1:].split('/')
 
 
 class CreateFrame:
     def __init__(self, topics):
         self.topics = topics
-        framework_of_files = self.create_framework(topics)
-        for f in framework_of_files:
-            print(f)
+        framework_of_files = self.second_framework(topics)
+
         #BuildTemplate(framework_of_files)
+
+
+    '''
+    LEFT HERE: remmeber to try to find a way to combine and check if the value already exists
+    '''
+    def second_framework(self, topics):
+        frame = {}
+
+        for per_topic in topics:
+            main_topic, sub_topic, *rest_topics = self.split_headerlist(per_topic)
+            main_topic = [main_topic]
+            totaltopics = main_topic + sub_topic
+            new_dict = current = {}
+
+            for sub_per_topic in totaltopics:
+                if sub_per_topic is not totaltopics[-1]:
+
+                    current[sub_per_topic] = {}
+                    current = current[sub_per_topic]
+                    frame[sub_per_topic] = current[sub_per_topic]
+                else:
+                    current[sub_per_topic] = []
+                    current = current[sub_per_topic]
+
+        print(frame)
+
+
+
 
 
     def create_framework(self, sub_headers_list):
@@ -223,7 +254,10 @@ class CreateFrame:
         for sub_header in sub_headers_list:
 
             main_topic, sub_topic, *rest_topics = self.split_headerlist(sub_header)
+
+
             check_validate = self.validate_topic(frame_d, main_topic, sub_topic)
+
 
             if (check_validate is True) and (rest_topics[0]):
                 title = self.title_of_topic(rest_topics[0])
@@ -241,6 +275,8 @@ class CreateFrame:
         test_first_val = first_header # generalize for the rest of the vals
         path, first_cat, *subtopic = test_first_val
         init_path_val = self.extract_first_val(path)
+
+
 
         return [init_path_val, first_cat, *subtopic]
 
@@ -287,7 +323,6 @@ class BuildTemplate:
 
 
         template.drawCentredString(250, 100, "TEMP")
-
         template.showPage()
         template.save()
 
